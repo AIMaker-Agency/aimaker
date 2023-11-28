0dfaa627e2b3009e099675bf9e7642872ed39810
Esteban Barraza <estebanbar1912@gmail.com>
2023-11-28 15:45:34 -0500

    Adding vite ssl plugin and host into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   test: {
     globals: true,
     environment: "jsdom",
@@ -19,5 +20,7 @@ export default defineConfig({
   },
   server: {
     port: 3005,
+    host: "0.0.0.0",
+    https: true,
   },
 });
